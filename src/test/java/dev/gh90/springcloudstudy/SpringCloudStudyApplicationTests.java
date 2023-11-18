379f9a03a5b4f51536bae6e530f625d9f571b605
ghkim <rmsgud0202@gmail.com>
2023-11-18 22:15:23 +0900

    first commit

---
+++ src/test/java/dev/gh90/springcloudstudy/SpringCloudStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gh90.springcloudstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
